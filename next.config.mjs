b8399b6a8bec38797e58cf3058299a4060d72d3f
virgoone <me@virgo.one>
2022-06-08 11:48:05 +0800

    fix: cors config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 export default {
+  headers: async () => {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ]
+  },
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   webpack: (config, { dev, isServer, ...options }) => {
@@ -7,9 +28,9 @@ export default {
     // and layout
     config.resolve.fallback = {
       ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
-        // by next.js will be dropped. Doesn't make much sense, but how it is
+      // by next.js will be dropped. Doesn't make much sense, but how it is
       fs: false, // the solution
-    };
+    }
 
     return config
   },
