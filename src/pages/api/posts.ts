bb4fdda5f36f6154b38008ce0a0ef9dbf9afd2ae
virgoone <me@virgo.one>
2022-06-08 11:22:45 +0800

    feat: 增加post api

---
+++ src/pages/api/posts.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getAllPosts, PostType } from '../../utils/posts'
+
+type Data = {
+  posts: PostType[]
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  const { limit, fields = 'title,date,description' } = req.query
+  const limitCount = parseInt(limit?.toString()) || 5
+  const posts = await getAllPosts(fields.toString().split(','))
+  res.status(200).json({
+    posts: (limitCount ? posts.splice(0, limitCount) : posts) as PostType[],
+  })
+}
