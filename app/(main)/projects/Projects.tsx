4fc655abe162b2fa2b5f13e3d20c1372aa914593
virgoone <w2008second@gmail.com>
2024-05-31 21:49:54 +0800

    fix: build error

--- app/(main)/projects/Projects.tsx
+++ app/(main)/projects/Projects.tsx
@@ -10,7 +10,7 @@ export async function Projects() {
       className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
     >
       {projects.map((project) => (
-        <ProjectCard project={project} key={project._id} />
+        <ProjectCard project={project} key={project.id} />
       ))}
     </ul>
   )
