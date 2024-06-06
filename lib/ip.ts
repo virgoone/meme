8fa95ff927d0ca6ad7cd868193cd0e7f56ba6036
virgoone <w2008second@gmail.com>
2024-06-06 10:35:41 +0800

    fix: update ip log

--- lib/ip.ts
+++ lib/ip.ts
@@ -2,10 +2,12 @@ import { type NextRequest } from 'next/server'
 
 export function getIP(request: Request | NextRequest): string {
   if ('ip' in request && request.ip) {
+    console.log('request.ip-->', request.ip)
     return request.ip
   }
 
   const xff = request.headers.get('x-forwarded-for')
+  console.log('xff.ip-->', xff)
   if (xff === '::1') {
     return '127.0.0.1'
   }
