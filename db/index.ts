fed379a2026be8aa6d6d388fa9f2ed353efc92f5
virgoone <w2008second@gmail.com>
2025-04-12 18:58:36 +0800

    fix: db client

--- db/index.ts
+++ db/index.ts
@@ -1,13 +1,12 @@
-import { createClient as createWebClient } from '@libsql/client/web'
-import { drizzle, LibSQLDatabase } from 'drizzle-orm/libsql'
-
+import { drizzle } from 'drizzle-orm/libsql';
 import { env } from '~/env.mjs'
 
 import * as schema from './schema'
 
 // create the connection
-const client = createWebClient({
-  url: env.TURSO_DB_URL || '',
-  authToken: env.TURSO_DB_AUTH_TOKEN || '',
-})
-export const db = drizzle(client, { schema })
+export const db = drizzle({
+  connection: {
+    url: env.TURSO_DB_URL!,
+    authToken: env.TURSO_DB_AUTH_TOKEN!,
+  },
+}, { schema })
