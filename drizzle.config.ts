a4cb9390de4b6219be63c73dd4baa0de33a8027e
virgoone <w2008second@gmail.com>
2025-04-13 12:23:02 +0800

    fix: udpate build error

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,4 +8,4 @@ export default {
   out: './db/migrations',
   dialect: 'sqlite',
   dbCredentials: { url: process.env.TURSO_DB_URL || '', authToken: process.env.TURSO_DB_AUTH_TOKEN || '' },
-} satisfies Config
+} as Config
